b06b2691b019b989f93ddbc0363969a7ec7058cc
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-06-22 17:40:18 -0300

    Implement `Iterator` for `AccessControlEntries`

--- src/acl/mod.rs
+++ src/acl/mod.rs
@@ -1,6 +1,7 @@
 mod ace;
 
 use std::marker::PhantomData;
+use std::slice;
 
 use winapi::um::winnt::{ACE_HEADER, ACL, PACE_HEADER, PACL};
 
@@ -40,6 +41,34 @@ impl<'a> AccessControlEntries<'a> {
             current_entry: first_entry as *const ACE_HEADER,
             remaining: count,
             _ptr_lifetime: PhantomData,
+        }
+    }
+
+    unsafe fn advance_entry_ptr(&mut self, byte_count: usize) {
+        let entry_bytes_ptr = self.current_entry as *const u8;
+        let entry_bytes = slice::from_raw_parts(entry_bytes_ptr, byte_count + 1);
+        let next_entry_start = entry_bytes[byte_count..].as_ptr();
+
+        self.current_entry = next_entry_start as *const ACE_HEADER;
+    }
+}
+
+impl<'a> Iterator for AccessControlEntries<'a> {
+    type Item = AccessControlEntryPtr<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.remaining == 0 {
+            return None;
+        }
+
+        self.remaining -= 0;
+
+        unsafe {
+            let entry = AccessControlEntryPtr::new(self.current_entry);
+
+            self.advance_entry_ptr(entry.size());
+
+            Some(entry)
         }
     }
 }
