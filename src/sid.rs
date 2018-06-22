81e21753ebb9deda4a12de3b0db9442769ea4af7
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-06-22 14:22:13 -0300

    Implement `Display` for `SecurityIdPtr`

--- src/sid.rs
+++ src/sid.rs
@@ -1,5 +1,10 @@
+use std::ffi::CStr;
+use std::fmt::{self, Display, Formatter};
 use std::marker::PhantomData;
+use std::ptr;
 
+use winapi::shared::sddl::ConvertSidToStringSidA;
+use winapi::um::winbase::LocalFree;
 use winapi::um::winnt::PSID;
 
 pub struct SecurityIdPtr<'a> {
@@ -12,6 +17,30 @@ impl<'a> SecurityIdPtr<'a> {
         SecurityIdPtr {
             sid,
             _ptr_lifetime: PhantomData,
+        }
+    }
+}
+
+impl<'a> Display for SecurityIdPtr<'a> {
+    fn fmt(&self, formatter: &mut Formatter) -> fmt::Result {
+        unsafe {
+            let mut sid_string_ptr = ptr::null_mut();
+
+            let convert_result = ConvertSidToStringSidA(self.sid, &mut sid_string_ptr);
+            if convert_result == 0 {
+                return Err(fmt::Error);
+            }
+
+            let sid_cstr = CStr::from_ptr(sid_string_ptr);
+            let sid = sid_cstr.to_string_lossy();
+
+            let fmt_result = formatter.write_str(&sid);
+
+            if !LocalFree(sid_string_ptr as *mut _).is_null() {
+                panic!("Failed to deallocate string");
+            }
+
+            fmt_result
         }
     }
 }
