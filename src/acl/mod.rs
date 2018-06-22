c4f61779dcd9711268fb5633d1514c196167d04b
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-06-22 17:33:41 -0300

    Create `AccessControlEntries` type

--- src/acl/mod.rs
+++ src/acl/mod.rs
@@ -2,7 +2,7 @@ mod ace;
 
 use std::marker::PhantomData;
 
-use winapi::um::winnt::{ACL, PACL};
+use winapi::um::winnt::{ACE_HEADER, ACL, PACE_HEADER, PACL};
 
 pub use self::ace::AccessControlEntryPtr;
 
@@ -24,6 +24,22 @@ impl<'a> AccessControlListPtr<'a> {
             let acl = self.acl as *const ACL;
 
             (*acl).AceCount as usize
+        }
+    }
+}
+
+pub struct AccessControlEntries<'a> {
+    current_entry: *const ACE_HEADER,
+    remaining: usize,
+    _ptr_lifetime: PhantomData<&'a ()>,
+}
+
+impl<'a> AccessControlEntries<'a> {
+    pub unsafe fn new(first_entry: PACE_HEADER, count: usize) -> Self {
+        AccessControlEntries {
+            current_entry: first_entry as *const ACE_HEADER,
+            remaining: count,
+            _ptr_lifetime: PhantomData,
         }
     }
 }
